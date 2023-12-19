1a2f3e29f91fac5b20a8d65900c1f012cbd41c3e
magixsource <magixsource.cn@gmail.com>
2023-12-19 21:00:33 +0800

    缓存disease\food\ingredient\foodMaterial查询结果

--- src/main/java/gl/linpeng/FunctionComputeProxyController.java
+++ src/main/java/gl/linpeng/FunctionComputeProxyController.java
@@ -1,13 +1,15 @@
 package gl.linpeng;
 
 
+import cn.hutool.cache.Cache;
+import cn.hutool.cache.CacheUtil;
+import cn.hutool.core.date.DateUnit;
 import com.alibaba.fastjson.JSON;
 import com.aliyun.fc.runtime.Context;
 import com.aliyun.fc.runtime.Credentials;
 import com.aliyun.fc.runtime.FunctionComputeLogger;
 import com.aliyun.fc.runtime.FunctionParam;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import gl.linpeng.serverless.advisor.service.impl.FoodServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.Map;
 
 
@@ -31,6 +34,8 @@ import java.util.Map;
 public class FunctionComputeProxyController {
     private static final Logger logger = LoggerFactory.getLogger(FunctionComputeProxyController.class);
 
+    private Cache<String, String> lfuCache = CacheUtil.newLFUCache(1000);
+    private String[] cacheFunctions = new String[]{"disease","food","ingredient","foodMaterialQuery"};
 
     @RequestMapping(value = "/proxy/{groupName}/{functionName}", method = RequestMethod.POST)
     public Object proxy(@PathVariable String functionName,@PathVariable String groupName, @RequestBody String postData,@RequestHeader Map header) throws ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException, IOException {
@@ -47,51 +52,65 @@ public class FunctionComputeProxyController {
         logger.info("Request      Header:"+header);
         logger.info("Request RequestBody:"+postData);
 
-        // get all Serverless
-        String className = serverlessPackage + "." + toCamelName(functionName) + "Controller";
-        Class clz = getClass().getClassLoader().loadClass(className);
-        Object instance = clz.newInstance();
-        Method[] methods = clz.getMethods();
-        Method handle = null;
-        for (Method method : methods) {
-            if (method.getName().equalsIgnoreCase("handleRequest")) {
-                handle = method;
-                break;
+        boolean isCacheable = Arrays.asList(cacheFunctions).contains(functionName);
+        String result = null;
+        Map<String,Object> requestBody = JSON.parseObject(postData, Map.class);
+        String cacheKey = "group:"+groupName+"-function:"+functionName+"-id:"+requestBody.get("id")+"-page:"+requestBody.get("page")+"-pageSize:"+requestBody.get("pageSize");
+        if(isCacheable && lfuCache.containsKey(cacheKey)){
+             result =  lfuCache.get(cacheKey);
+        }else{
+            // get all Serverless
+            String className = serverlessPackage + "." + toCamelName(functionName) + "Controller";
+            Class clz = getClass().getClassLoader().loadClass(className);
+            Object instance = clz.newInstance();
+            Method[] methods = clz.getMethods();
+            Method handle = null;
+            for (Method method : methods) {
+                if (method.getName().equalsIgnoreCase("handleRequest")) {
+                    handle = method;
+                    break;
+                }
+            }
+            if (handle == null) {
+                throw new RuntimeException("Can't find serverless handle method.");
+            }
+
+            Context ctx = new Context() {
+                @Override
+                public String getRequestId() {
+                    return String.valueOf(System.currentTimeMillis());
+                }
+
+                @Override
+                public Credentials getExecutionCredentials() {
+                    return null;
+                }
+
+                @Override
+                public FunctionParam getFunctionParam() {
+                    return null;
+                }
+
+                @Override
+                public FunctionComputeLogger getLogger() {
+                    return null;
+                }
+            };
+            Class[] types = handle.getParameterTypes();
+            ObjectMapper mapper = new ObjectMapper();
+            Type type = clz.getGenericSuperclass();
+            ParameterizedType parameterizedType = (ParameterizedType) type;
+            Class actualClass = (Class) parameterizedType.getActualTypeArguments()[0];
+            Object dto = mapper.readValue(postData, actualClass);
+            Object jsonObject = handle.invoke(instance, dto, ctx);
+            result = JSON.toJSONString(jsonObject);
+            // cache result
+            if(isCacheable){
+                lfuCache.put(cacheKey,result, DateUnit.HOUR.getMillis() * 24);
             }
         }
-        if (handle == null) {
-            throw new RuntimeException("Can't find serverless handle method.");
-        }
 
-        Context ctx = new Context() {
-            @Override
-            public String getRequestId() {
-                return String.valueOf(System.currentTimeMillis());
-            }
-
-            @Override
-            public Credentials getExecutionCredentials() {
-                return null;
-            }
-
-            @Override
-            public FunctionParam getFunctionParam() {
-                return null;
-            }
-
-            @Override
-            public FunctionComputeLogger getLogger() {
-                return null;
-            }
-        };
-        Class[] types = handle.getParameterTypes();
-        ObjectMapper mapper = new ObjectMapper();
-        Type type = clz.getGenericSuperclass();
-        ParameterizedType parameterizedType = (ParameterizedType) type;
-        Class actualClass = (Class) parameterizedType.getActualTypeArguments()[0];
-        Object dto = mapper.readValue(postData, actualClass);
-        Object result = handle.invoke(instance, dto, ctx);
-        logger.info("Response       Body:"+ JSON.toJSONString(result));
+        logger.info("Response       Body:"+ result);
         logger.info("=====================end.");
         return result;
     }
